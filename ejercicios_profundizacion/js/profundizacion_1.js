7cdadc790dff224e3b59e62d28c61fb36d83d003
Fdvalls <fernandodanielvalls@gmail.com>
2023-05-22 09:50:30 -0300

    corrección variables var ej1 profundizacion

--- ejercicios_profundizacion/js/profundizacion_1.js
+++ ejercicios_profundizacion/js/profundizacion_1.js
@@ -13,30 +13,38 @@
  *    medio de alert(); y console.log(); el detalle de los datos recibidos.
 */
 
-var num1;
-var num2;
-var num3;
-var num4;
-var num5;
-
 function inputData() {
-    var input1 = prompt("Ingrese primer numero");
-    num1 = parseFloat(input1);
-    var input2 = prompt("Ingreses segundo numero");
-    num2 = parseFloat(input2);
-    var input3 = prompt("Ingrese tercer numero");
-    num3 = parseFloat(input3);
-    var input4 = prompt("Ingrese cuarto numero");
-    num4 = parseFloat(input4);
-    var input5 = prompt("Ingreses quinto numero");
-    num5 = parseFloat(input5);
+    let myArray = [];
+    let input1 = prompt("Ingrese primer numero");
+    let num1 = parseFloat(input1);
+    let input2 = prompt("Ingreses segundo numero");
+    let num2 = parseFloat(input2);
+    let input3 = prompt("Ingrese tercer numero");
+    let num3 = parseFloat(input3);
+    let input4 = prompt("Ingrese cuarto numero");
+    let num4 = parseFloat(input4);
+    let input5 = prompt("Ingreses quinto numero");
+    let num5 = parseFloat(input5);
+
+    myArray.push(num1, num2, num3, num4, num5)
+
+    return myArray
 }
 
-function promedio(num1, num2, num3, num4, num5) {
-    let sum = num1 + num2 + num3 + num4 + num5;
-    let prom = sum / 5;
-    console.log("Los datos ingresados son: " + num1, num2, num3, num4, num5);
+let listaNumeros = inputData();
+
+function promedio(listaNumeros) {
+    let prom = 0;
+    let sum = 0;
+    let cont = 0;
+    listaNumeros.forEach(e => {
+        cont += 1
+        sum += e;
+        prom = sum / cont;
+
+    });
+    console.log("Los datos ingresados son: " + listaNumeros[0], listaNumeros[1], listaNumeros[2], listaNumeros[3], listaNumeros[4]);
     console.log("El promedio de los 5 números es: " + prom);
-    alert("Los datos ingresados son: " + "\n" + num1 + "\n" + num2 + "\n" + num3 + "\n" + num4 + "\n" + num5);
+    alert("Los datos ingresados son: " + "\n" + listaNumeros[0] + "\n" + listaNumeros[1] + "\n" + listaNumeros[2] + "\n" + listaNumeros[3] + "\n" + listaNumeros[4]);
     alert("El promedio de los 5 números es: " + prom);
 }
